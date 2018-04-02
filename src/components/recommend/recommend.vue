460312d777f2356dde25d0347b5a41380ba4bd86
kzz710 <710541690@qq.com>
2018-04-03 00:08:49 +0800

    second commit

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,17 +1,72 @@
 <template>
     <div class="recommend">
-      这是推荐页面
+      <div class="recommend-content">
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider-component>
+            <div v-for="(item, index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" alt="">
+              </a>
+            </div>
+          </slider-component>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul></ul>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
+  import SliderComponent from 'base/slider/slider'
+  import {getRecommend} from 'api/recommend.js'
+  import {ERR_OK} from 'api/config.js'
   export default {
     name: 'recommend',
+    data () {
+      return {
+        recommends: []
+      }
+    },
+    components: {
+      SliderComponent
+    },
     created () {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend () {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+              this.recommends = res.data.slider
+          }
+        })
+      }
     }
   }
 </script>
 
-<style>
-
+<style lang="stylus" type="text/stylus">
+  @import "../../common/stylus/variable.styl"
+  .recommend {
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content {
+      position: relative
+      width: 100%
+      overflow hidden
+      .recommend-list {
+        .list-title {
+          height: 65px
+          line-height: 65px
+          text-align center
+          font-size $font-size-medium
+          color $color-theme
+        }
+      }
+    }
+  }
 </style>
